7c505cf016e1cda58c88a29837bcbd5d7772aaf6
HKPHAM <huykhoipham2010@gmail.com>
2025-04-15 10:44:01 +0700

    Delete Domain

--- BookStore.Domain/Entities/RefreshToken.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BookStore.Application.Entities;
-public class RefreshToken
-{
-    public string Token { get; set; }
-    public int UserId { get; set; }
-    public DateTime ExpiryDate { get; set; }
-}
